2a43b4b1b7aaaf914de9c0e61f5e4e8e8ddc947f
emilpsyv <tu7wu02zm@code.edu.az>
2024-05-07 01:30:51 +0400

    first commit

---
+++ ProniaEmil/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProniaEmil.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task <IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Shop()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Blog()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AboutUs()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Pages()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ContactUs()
+        {
+            return View();
+        }
+    }
+}
